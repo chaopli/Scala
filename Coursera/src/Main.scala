d2bef00d460349d08e3157c8e6291e48778e3c1f
Chao Li <lichaorodxx@gmail.com>
2015-06-28 23:31:03 -0700

    Finished Scala basic sytax

--- Coursera/src/Main.scala
+++ Coursera/src/Main.scala
@@ -1,6 +1,7 @@
 import week1.Factorial
 import week1.Sum
 import week2.Product
+import week2.lecture2_3
 
 /**
  * @author chaoli
@@ -9,5 +10,6 @@ object Main {
   def main(args: Array[String]) {
   	println(Factorial.calc(10));
   	println(Product.calc(x=>x*x)(3, 4));
+  	println(lecture2_3.sqrt(2));
   }
 }
